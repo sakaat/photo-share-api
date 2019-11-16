c05f06e1982b43dd88e1d49689328500a30eb24d
sakata.tomoyuki <a1819ts@aiit.ac.jp>
2019-11-17 01:29:52 +0900

    5.2.1 Root Resolvers

--- index.ts
+++ index.ts
@@ -1,1 +1,33 @@
-console.log("index");
+import { ApolloServer } from "apollo-server";
+
+const typeDefs = `
+    type Query {
+        totalPhotos: Int!
+    }
+    type Mutation {
+        postPhoto(name: String! description: String): Boolean!
+    }
+`;
+
+const photos = [];
+
+const resolvers = {
+    Query: {
+        totalPhotos: () => photos.length,
+    },
+    Mutation: {
+        postPhoto(_parent, args) {
+            photos.push(args);
+            return true;
+        },
+    },
+};
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+server
+    .listen()
+    .then(({ url }) => console.log(`GraphQL Service running on ${url}`));
