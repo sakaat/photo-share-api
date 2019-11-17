58052a582e81dc4f8a22eec9947a5533abe98701
sakata.tomoyuki <a1819ts@aiit.ac.jp>
2019-11-17 16:55:20 +0900

    5.5.3 githubAuth Mutation

--- resolvers/Mutation.ts
+++ resolvers/Mutation.ts
@@ -1,3 +1,4 @@
+import { authorizeWithGithub } from "../lib";
 import { photos } from "./index";
 
 module.exports = {
@@ -10,5 +11,40 @@ module.exports = {
         };
         photos.push(newPhoto);
         return newPhoto;
+    },
+
+    async githubAuth(_parent, { code }, { db }) {
+        const {
+            message,
+            access_token,
+            avatar_url,
+            login,
+            name,
+        } = await authorizeWithGithub({
+            client_id: process.env.CLIENT_ID,
+            client_secret: process.env.CLIENT_SECRET,
+            code,
+        });
+
+        if (message) {
+            throw new Error(message);
+        }
+
+        const latestUserInfo = {
+            name,
+            githubLogin: login,
+            githubToken: access_token,
+            avatar: avatar_url,
+        };
+
+        const {
+            ops: [user],
+        } = await db
+            .collection("users")
+            .replaceOne({ githubLogin: login }, latestUserInfo, {
+                upsert: true,
+            });
+
+        return { user, token: access_token };
     },
 };
