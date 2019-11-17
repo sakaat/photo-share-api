82153a1634c1b375a31213a870cb2b492c5f7b59
sakata.tomoyuki <a1819ts@aiit.ac.jp>
2019-11-17 11:07:42 +0900

    5.2.4 Edges and Connections

--- index.ts
+++ index.ts
@@ -8,12 +8,19 @@ const typeDefs = `
         LANDSCAPE
         GRAPHIC
     }
+    type User {
+        githubLogin: ID!
+        name: String
+        avatar: String
+        postedPhotos: [Photo!]!
+    }
     type Photo {
         id: ID!
         url: String!
         name: String!
         description: String
         category: PhotoCategory!
+        postedBy: User!
     }
     type Query {
         totalPhotos: Int!
@@ -30,7 +37,35 @@ const typeDefs = `
 `;
 
 let _id = 0;
-const photos = [];
+
+const users = [
+    { githubLogin: "mHattrup", name: "Mike Hattrup" },
+    { githubLogin: "gPlake", name: "Glen Plake" },
+    { githubLogin: "sSchmidt", name: "Scot Schmidt" },
+];
+
+const photos = [
+    {
+        id: "1",
+        name: "Dropping the Heart Chute",
+        description: "The heart chute is one of my favorite chutes",
+        category: "ACTION",
+        githubUser: "gPlake",
+    },
+    {
+        id: "2",
+        name: "Enjoying the sunshine",
+        category: "SELFIE",
+        githubUser: "sSchmidt",
+    },
+    {
+        id: "3",
+        name: "Gunbarrel 25",
+        description: "25 laps on gunbarrel today",
+        category: "LANDSCAPE",
+        githubUser: "sSchmidt",
+    },
+];
 
 const resolvers = {
     Query: {
@@ -47,8 +82,16 @@ const resolvers = {
             return newPhoto;
         },
     },
+    User: {
+        postedPhotos: (parent) => {
+            return photos.filter((p) => p.githubUser === parent.githubLogin);
+        },
+    },
     Photo: {
         url: (parent) => `https://yoursite.com/img/${parent.id}.jpg`,
+        postedBy: (parent) => {
+            return users.find((u) => u.githubLogin === parent.githubUser);
+        },
     },
 };
 
